53eb79f05d4ad3850d85bab859c4ed0e83ac1cee
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-19 19:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Add your code here
 function submitData(name, email){
   
-  return fetch();
+  return fetch("http://localhost:3000/users", configurationObject = {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
+      "Accept" : "application/json"
+    },
+    body : JSON.stringify({
+      
+    })
+  });
   
 }
