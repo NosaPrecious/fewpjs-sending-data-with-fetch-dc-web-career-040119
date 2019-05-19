44a0bc51e358e70582edf30dbf90ae4c3dbc1fc9
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-19 19:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function submitData(name, email){
       "Accept" : "application/json"
     },
     body : JSON.stringify({
-      username : name
+      username : name,
       email : email
     })
   }
